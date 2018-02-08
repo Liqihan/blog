154c1209051c1ac7d662e7fb46c31752ec50303d
李奇涵 <liqihan@benditoutiao.com>
2018-02-08 23:42:35 +0800

    promise + 文章

--- test/promise-test.js
+++ test/promise-test.js
@@ -1,14 +1,63 @@
-var Promise = require('../lib/Promise');
+var Promise = require("../lib/Promise");
 console.log(Promise);
 var a = new Promise(function(resolve, reject) {
     console.log(1111);
     resolve();
-}).then(function() {
-    console.log(1211);
-}).then(() => {
-    console.log(333);
 })
-Promise.resolve('234234').then((data) => {
+    .then(function() {
+        console.log(1211);
+    })
+    .then(() => {
+        console.log(333);
+    });
+Promise.resolve("234234").then(data => {
     console.log(1231231231232);
     console.log(data);
-})
+});
+function doSomething() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("something");
+        }, 1000);
+    });
+}
+
+function doSomethingElse() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("somethingElse");
+        }, 1500);
+    });
+}
+console.time("case 1");
+doSomething()
+    .then(() => {
+        return doSomethingElse();
+    })
+    .then(function finalHandler(res) {
+        console.log(res);
+        console.timeEnd("case 1");
+    });
+console.time("case 2");
+doSomething()
+    .then(function() {
+        doSomethingElse();
+    })
+    .then(function finalHandler(res) {
+        console.log(res);
+        console.timeEnd("case 2");
+    });
+console.time("case 3");
+doSomething()
+    .then(doSomethingElse())
+    .then(function finalHandler(res) {
+        console.log(res);
+        console.timeEnd("case 3");
+    });
+console.time("case 4");
+doSomething()
+    .then(doSomethingElse)
+    .then(function finalHandler(res) {
+        console.log(res);
+        console.timeEnd("case 4");
+    });
